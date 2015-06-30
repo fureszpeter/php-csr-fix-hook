55a02cd696d6661f78f15062fa8f76c9c35543df
Furesz Peter <fureszpeter@gmail.com>
2015-06-30 01:34:06 +0100

    Finish

--- src/Git/Git.php
+++ src/Git/Git.php
@@ -2,11 +2,41 @@
 namespace GitHook\Git;
 
 
-class Git {
-
-
+class Git
+{
+    /**
+     * @return string
+     */
     public function getCurrentBranch()
     {
-        return 'master';
+        {
+            $output = $this->execute('git symbolic-ref HEAD');
+
+            $tmp = explode('/', $output[0]);
+
+            return $tmp[2];
+        }
+    }
+
+    /**
+     * @param $string
+     *
+     * @return array
+     */
+    private function execute($string)
+    {
+        exec($string, $output);
+
+        return $output;
+    }
+
+    /**
+     * @return array List of files
+     */
+    public function getCommitAffectedFiles()
+    {
+        exec('git diff --cached --name-only --diff-filter=ACM', $output);
+
+        return $output;
     }
 }
--- tests/GitTest.php
+++ tests/GitTest.php
@@ -13,13 +13,57 @@ class GitTest extends PHPUnit_Framework_TestCase
     public function testGetCurrentBranch()
     {
         $testRepoPath = $this->initGitDirectory('phpunit');
+        chdir($testRepoPath);
 
         $git = new Git($testRepoPath);
-        $branch = $git->getCurrentBranch();
 
-        $this->assertEquals('master', $branch);
+        $this->assertEquals('master', $git->getCurrentBranch());
+
+        exec('touch README.md && git add README.md && git commit -m "initial commit" && git branch temp && git checkout temp');
+        $this->assertEquals('temp', $git->getCurrentBranch());
+
+        unlink('README.md');
 
         $this->safeDeleteDir($testRepoPath);
+    }
+
+    /**
+     * @param ...$files
+     *
+     * @dataProvider filenameProvider
+     *
+     * @return void
+     */
+    public function testCommitFiles(...$files)
+    {
+        $testRepoPath = $this->initGitDirectory('phpunit');
+        $this->workDir = $testRepoPath;
+
+        chdir($testRepoPath);
+
+        $git = new Git($testRepoPath);
+        foreach ($files as $file) {
+            exec('touch ' . $file);
+            exec('git add ' . $file);
+        }
+
+        $commitFiles = $git->getCommitAffectedFiles();
+        $this->assertEquals($files, $commitFiles);
+
+        foreach ($files as $file) {
+            unlink($file);
+        }
+
+        $this->safeDeleteDir($testRepoPath);
+
+    }
+
+    public function filenameProvider()
+    {
+        return [
+            ['mockFile.txt'],
+            ['mockFile2.txt', 'mockFile3.txt'],
+        ];
     }
 
     /**
@@ -31,12 +75,13 @@ class GitTest extends PHPUnit_Framework_TestCase
     public function initGitDirectory($dirName)
     {
         $repoPath = sys_get_temp_dir() . '/' . $dirName;
+        $this->workDir = $repoPath;
 
-        if (! is_dir($repoPath) && ! mkdir($repoPath, 0777, true)) {
+        if (!is_dir($repoPath) && !mkdir($repoPath, 0777, true)) {
             throw new RuntimeException('Unable to init git in temp dir: ' . $repoPath);
         }
 
-        if (! chdir($repoPath)) {
+        if (!chdir($repoPath)) {
             throw new RuntimeException('Unable to change workdir to ' . $repoPath);
         }
 
@@ -50,7 +95,8 @@ class GitTest extends PHPUnit_Framework_TestCase
      */
     private function safeDeleteDir($dirName)
     {
-        if (is_dir($this->workDir)) {
+
+        if (is_dir($dirName)) {
             exec("rm -rf {$dirName}/.git && rmdir {$dirName}");
         }
 
@@ -59,7 +105,7 @@ class GitTest extends PHPUnit_Framework_TestCase
     public function __destruct()
     {
         if (is_dir($this->workDir)) {
-            exec("rm -rf {$this->workDir}/.git && rmdir {$this->workDir}");
+            exec("rm -rf {$this->workDir}/.git && rm -rf {$this->workDir}");
         }
     }
 }
